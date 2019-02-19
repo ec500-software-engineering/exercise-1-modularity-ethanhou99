25fbca27bdaae7e88650c3debe2a36e9a864caa8
Yicun Hou <yicunhou@bu.edu>
2019-02-19 17:25:03 -0500

    syntax change

--- OutputAlert.py
+++ OutputAlert.py
@@ -7,17 +7,17 @@ def receive_basic_iuput_data(Singal_Loss, Shock_Alert, Oxygen_Supply, Fever, Hyp
  ##Boolean Parameters
  ##If paramter returns True, means it should be alerted, then add it to the array
     BasicResult = {'Signal_Loss':False, 'Shock_Alert':False,'Oxygen_Supply':False,'Fever':False,'Hypotension':False,'Hypertension':False}
-    if(Singal_Loss == True):
+    if(Singal_Loss is True):
         BasicResult['Signal Loss']=True
-    if(Shock_Alert == True):
+    if(Shock_Alert is True):
         BasicResult['Shock_Alert']=True 
-    if(Oxygen_Supply == True):
+    if(Oxygen_Supply is True):
         BasicResult['Oxygen_Supply']=True 
-    if(Fever == True):
+    if(Fever is True):
         BasicResult['Fever']=True
-    if(Hypotension == True):
+    if(Hypotension is True):
         BasicResult['Hypotension']=True
-    if(Hypertension == True):
+    if(Hypertension is True):
         BasicResult['Hypertension']=True 
 
     return BasicResult
@@ -50,32 +50,32 @@ def print_title(patientID, gender, age):
 def alert_management(Systolic_BP, Diastolic_BP, Heart_Rate, Heart_O2_Level, Body_temp):
     new_alert = analysis.Analyzer(Systolic_BP, Diastolic_BP, Heart_Rate, Heart_O2_Level, Body_temp)
         
-    if (new_alert.Shock_Alert(int(Heart_Rate), int(Body_temp)) == True or\
-        new_alert.Signal_Loss(int(Heart_Rate), int(Body_temp)) == True or\
-        new_alert.Oxygen_Supply(int(Heart_O2_Level)) == True or\
-        new_alert.Fever(int(Body_temp)) == True or\
-        new_alert.Hypotension(int(Systolic_BP), int(Diastolic_BP)) == True or\
-        new_alert.Hypertension(int(Systolic_BP), int(Diastolic_BP)) == True):
+    if (new_alert.Shock_Alert(int(Heart_Rate), int(Body_temp)) is True or\
+        new_alert.Signal_Loss(int(Heart_Rate), int(Body_temp)) is True or\
+        new_alert.Oxygen_Supply(int(Heart_O2_Level)) is True or\
+        new_alert.Fever(int(Body_temp)) is True or\
+        new_alert.Hypotension(int(Systolic_BP), int(Diastolic_BP)) is True or\
+        new_alert.Hypertension(int(Systolic_BP), int(Diastolic_BP)) is True):
         print('\n\033[1;31;40m|Alert|\033[0m')
     else:
         print('\033[1;32mAll the vital signs were within normal limits.\033[0m\n')
 
-    if new_alert.Shock_Alert(int(Heart_Rate), int(Body_temp)) == True:
+    if new_alert.Shock_Alert(int(Heart_Rate), int(Body_temp)) is True:
         print('\033[1;31;40mShock_Alert!\033[0m')
 
-    if new_alert.Signal_Loss(int(Heart_Rate), int(Body_temp)) == True:
+    if new_alert.Signal_Loss(int(Heart_Rate), int(Body_temp)) is True:
         print('\033[1;31;40mWarning: Signal_Loss!\033[0m')
 
-    if new_alert.Oxygen_Supply(int(Heart_O2_Level)) == True:
+    if new_alert.Oxygen_Supply(int(Heart_O2_Level)) is True:
         print('\033[1;31;40mPlease Increase Oxygen_Supply!\033[0m')
 
-    if new_alert.Fever(int(Body_temp)) == True:
+    if new_alert.Fever(int(Body_temp)) is True:
         print('\033[1;31;40mFever!\033[0m')
 
-    if new_alert.Hypotension(int(Systolic_BP), int(Diastolic_BP)) == True:
+    if new_alert.Hypotension(int(Systolic_BP), int(Diastolic_BP)) is True:
         print('\033[1;31;40mHypotension\033[0m')
 
-    if new_alert.Hypertension(int(Systolic_BP), int(Diastolic_BP)) == True:
+    if new_alert.Hypertension(int(Systolic_BP), int(Diastolic_BP)) is True:
         print('\033[1;31;40mHypertension\033[0m')
 
 def display_AI_iuput_data(Blood_oxygen, Blood_pressure, Pulses):
